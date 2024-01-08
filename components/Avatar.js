a0a80c17334ea5b49d3690e64b6186f2a7936d17
Edmund <edmundjet99@gmail.com>
2024-01-07 18:12:09 -0800

    completed home page, updated profile picture for landing page

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,18 @@
+//next image
+import Image from "next/image";
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className="hidden xl:flex xl:max-w-none">
+      <Image
+        src={"/ed.png"}
+        width={433} // change width and height based on resolution of picture
+        height={577}
+        alt=""
+        className="translate-z-0 object-none w-full h-full rounded-full"
+      />
+    </div>
+  );
 };
 
 export default Avatar;
