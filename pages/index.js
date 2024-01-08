a46a590467d98b3c736001b2f5008bfb153824b6
Edmund <edmundjet99@gmail.com>
2024-01-07 19:54:32 -0800

    add logo

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ const Home = () => {
         {/* Background_Image */}
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
         {/* Paticles */}
-        <ParticlesContainer />
+        {/* <ParticlesContainer /> */}
         {/* Avatar Image */}
         <motion.div
           variants={fadeIn("up", 0.5)}
